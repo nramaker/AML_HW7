363062ede144da4e474d7181ffb4b33ed78a7b01
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2018-04-06 10:07:33 -0500

    EM translation attempt

--- TopicModeling.py
+++ TopicModeling.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from random import *
+from scipy.stats import multinomial
 
 k = 30
 
@@ -11,7 +13,7 @@ def load_vocab():
 
 def load_data():
     file = "./NIPS/docword.nips.txt"
-    print("   Loading vocab from file: {}".format(file))
+    print("   Loading data from file: {}".format(file))
     head = pd.read_csv(file, header=None, nrows=3).as_matrix()
     d = head[0][0]
     n = head[1][0]
@@ -19,9 +21,9 @@ def load_data():
     data = pd.read_csv(file, header=None, skiprows=3, sep=' ').as_matrix()
     return data, d, n, t
 
-def build_word_counts(data, d, t):
+def build_word_counts(data, d, n, t):
     print("   Building word_count vectors.")
-    word_counts = np.zeros((d, t))
+    word_counts = np.zeros((d, n))
 
     for i in range (0, t):
         row = data[i]
@@ -31,13 +33,13 @@ def build_word_counts(data, d, t):
         word_counts[docID-1][wordId-1] = word_counts[docID-1][wordId-1] + count
     return word_counts
 
-def expectation(points, cluster_centers, cluster_weights):
-    #TODO: implement estimation
-    return []
+# def expectation(points, cluster_centers, cluster_weights):
+#     #TODO: implement estimation
+#     return []
 
-def maximization(points, w_ij, t):
-    #TODO implement maximization
-    return np.asarray([]), np.asarray([])
+# def maximization(points, w_ij, t):
+#     #TODO implement maximization
+#     return np.asarray([]), np.asarray([])
 
 def show_histogram(weights):
     #TODO display histogram
@@ -47,10 +49,10 @@ def show_table(word_probs):
     #TODO display table
     pass
 
-def measure_change(old_mus, new_mus):
-    dist = np.linalg.norm(new_mus-old_mus)
-    #should we take the average change?
-    return dist
+# def measure_change(old_mus, new_mus):
+#     dist = np.linalg.norm(new_mus-old_mus)
+#     #should we take the average change?
+#     return dist
 
 if __name__ == "__main__":
     print("##### HW7 Topic Modeling #####")
@@ -60,36 +62,122 @@ if __name__ == "__main__":
     data, d, n, t = load_data()
 
     #organize the data into word counts per document
-    word_counts = build_word_counts(data, d, t)
+    word_counts = build_word_counts(data, d, n, t)
+
+    rows = word_counts.shape[0]  # word indexes per document
+    cols = word_counts.shape[1]  # documents indexes
+    # print("   rows {} : columns {}".format(rows, cols))
 
     #initialize variables
     #TODO: better initialization
-    w_ij  = []
-    mus = []
-    pis = [[1/k]] * k
+    w_ij  = np.zeros((k,rows))
+    
+    #these are the random initial cluster centers
+    np.random.seed(1)
+    mus = np.asarray([word_counts[np.random.randint(0, cols-1)]]*k) #randomly choose k documents as centroids
+
+    #these are the inital cluster weights
+    pis = [[1/k]] * k  #equally weighted clusters to start
+
 
     #perform clustering
     max_iters = 1000
-    min_change = 0.001
+    min_tau = 0.001
 
     iteration = 0
     converged = False
     while( iteration < max_iters and converged==False):
         print("### Performing E/M algorithm: iteration {}".format(iteration))
 
-        #e step
-        w_ij = expectation(word_counts, mus, pis)
+        #this is the old code, delete?
+        # #e step
+        # w_ij = expectation(word_counts, mus, pis)
 
-        #m step
+        # #m step
+        # old_mus = mus
+        # mus, pis = maximization(word_counts, w_ij, n)
+
+        # E - step
+        print("   E-step")
+        priors_probs = np.zeros((k,rows))
+
+        print("     Calculating Priors.")
+        for i in range(0,k-1):
+            for j in range(0,rows-1):
+                priors_probs[i,j] = multinomial.pmf(word_counts[j,], sum(word_counts[j,]), mus[i,])
+    
+        w_ij_new = np.zeros((k,rows))
+        sumcp = np.dot(np.asarray(pis).T, priors_probs)
+
+        sumcp = sumcp + 0.0001 #avoid divide by zero ?
+        
+        #new assignments
+        print("     Calculating New Assignments.")
+        for i in range(0,k-1):
+            w_ij_new[i,] = (np.asarray(pis[i]).T * priors_probs[i,])/sumcp
+        
+        # M - step
+        print("   M-Step")
         old_mus = mus
-        mus, pis = maximization(word_counts, w_ij, n)
+        for i in range(0, k-1):
+            pis[i] = sum(w_ij[i,])/rows
+            mus[i,] = (np.dot(w_ij[i,], word_counts))/sum(word_counts[i,])
+            mus[i,] = mus[i,]/n #normalize 
 
-        change = measure_change(old_mus, mus)
-        print("   Detected cluster center change of {}".format(change))
-        if(change < min_change):
+        print("  Measuring Tau")
+
+        # sum1 = sum(w_ij[0])
+        # print("sum1 {}".format(sum1))
+        # sum2 = sum(w_ij_new[0])
+        # print("sum2 {}".format(sum2))
+
+        #TODO: there is a defect here, tau evaluates to NaN
+        tau = np.linalg.norm(w_ij-w_ij_new)
+        print("   Detected probabilities change of {}".format(tau))
+        if(tau < min_tau):
             converged=True
             print("   EM algorithm has converged, stopping.")
+        w_ij=w_ij_new
         iteration+=1
+            
+    
+        #c is the cluster weights :  pis
+        #t is the cluster centers :  mus
+        #a is the cluster assignements: w_ij
+        #p is prior cluster probs : priors_probs 
+        #H is our data : word_counts
+
+        # E - step
+#   while(1){
+#     p = matrix(0,k,rows)
+#     for(i in 1:k){
+#       for(j in 1:rows){
+#         p[i,j] = dmultinom(H[j,],sum(H[j,]),t[i,])
+#       }
+#     }
+#     anew = matrix(0,k,rows)
+#     sumcp = c %*% p
+#     #print(dim(anew))
+#     #print(dim(sumcp))
+#     #print(dim(p))
+#     sumcp = sumcp + 0.0001
+#     for(i in 1:k){
+#       anew[i,] = (c[i] * p[i,])/sumcp
+#     }
+#     #check anew and old !!
+#     print(iter)
+#     #print(anew)
+#     print(norm((a-anew),"O"))
+#     if(norm((a-anew),"O") < tau)
+#       break
+#     a = anew
+    
+#     # M - step
+#     for(i in 1:k){
+#       c[i] = sum(a[i,])/rows
+#       t[i,] = (a[i,] %*% as.matrix(H))/sum(a[i,])
+#       t[i,] = t[i,]/12419
+#     }
     
     show_histogram(pis)
     show_table(w_ij)
