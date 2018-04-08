3a630f1ccd7e62ed00f2904d0855620ec43c0f82
Nathan Ramaker <nathan.ramaker@helpsystems.com>
2018-04-08 10:35:37 -0500

    notebook

--- TopicModeling.py
+++ TopicModeling.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #TODO: set this back to 30. Setting k=2 so that it takes less time while we iterate
 #k = 30
-k=3
+k=10
 
 def load_vocab():
     file = "./NIPS/vocab.nips.txt"
@@ -15,25 +15,31 @@ def load_vocab():
     return data
 
 def load_data():
-    file = "./NIPS/docword.nips.txt"
-    data_file = "./NIPS/nips.txt"
-    print("   Loading data from file: {}".format(file))
-    head = pd.read_csv(file, header=None, nrows=3).as_matrix()
-    d = head[0][0]
-    n = head[1][0]
-    t = head[2][0]
-    nips = pd.read_csv(data_file,delimiter=' ')
+    nips = pd.read_csv('./NIPS/nips.txt',delimiter=' ')
     nips = nips.pivot(columns='wordid', index='docid', values='count')
-    nips.fillna(.001, inplace=True)
-    return nips.as_matrix(), d, n, t
+    nips = nips.fillna(value=0)
+    nips = np.add(nips,0.1)
+
+    badcols = list(np.flatnonzero(np.sum(nips.values, 1)<1000))
+    allcols = list(range(1,1491))
+    cols = [x for x in allcols if x not in badcols]
+    nips = nips.iloc[:,cols]
+    nips.columns = list(range(0,nips.shape[1]))
+    #nips = nips.add(1)
+    nips_data = nips.values
+
+    print(str(nips.shape[0]) + " unique documents")
+    print(str(nips.shape[1]) + " unique words")
+    nips.head()
+    return nips
 
 def show_histogram(weights):
     plt.figure(1)
     
-    indexes = list(range(0,len(weights)))
+    indexes = list(range(0,weights.shape[1]))
     print("indexes : {}".format(indexes))
     print("weights : {}".format(weights))
-    plt.bar(indexes, weights, label=indexes)
+    plt.bar(indexes, weights[0], label=indexes)
      
     plt.xscale('linear')
     plt.yscale('linear')
@@ -72,33 +78,36 @@ def e_step(data,mu,k,pi):
     
     n = data.shape[0]
     d = data.shape[1]
-    
+
     h1 = np.multiply(data,data)
     h1 = np.matmul(h1,np.ones((d,k)))
-    
     h2 = np.matmul(np.ones((n,d)), np.multiply(mu,mu).T)
-    
     h3 = np.matmul(data,mu.T)
-    
     H = -0.5*(np.subtract(np.add(h1,h2),2*h3))
+
     P = np.matmul(np.ones((n,1)),pi.T)
     E = np.multiply(np.exp(H),P)
     F = np.matmul(E,np.ones((k,k)))
     F = np.add(F,0.01)
     W = np.divide(E,F)
+
     return(W)
 
-def m_step(W,data):
-    # W is a nxk array of document likelihoods (given cluster)
-    # data is a dxn array of word counts
-    D = np.matmul(W.T,data)
-    G = np.matmul(W.T,np.ones((n,d)))
-    mu_new = np.divide(D,G)
-    return(mu_new)
+def m_step_log(W,data,pis,alpha):
+    n = data.shape[0]
+    d = data.shape[1]
+    pi_new = np.log(np.matmul(W.T,np.ones((n,1))))
+    pi_new = np.subtract(pi_new, np.log(n))
+    W = np.multiply(alpha,W)
+    mu_new = np.matmul(W.T,data)
+    mu_new = np.divide(mu_new,np.matmul(W.T,np.ones((n,d))))
+    pi_new = np.matmul(W.T,np.ones((n,1)))
+    pi_new = np.divide(pi_new,n)
+    return(mu_new,pi_new)
     
 def evalulate(mu_old,mu_new,threshold):
     diff = np.subtract(mu_new,mu_old)
-    #diff = np.linalg.norm(diff)
+    diff = np.linalg.norm(diff)
     print("   Detected change of {}".format(diff))
     if diff <= threshold:
         return(1)
@@ -110,7 +119,7 @@ if __name__ == "__main__":
 
     #load data from files
     vocab = load_vocab()
-    nips_data, d, n, t = load_data() # use old load method
+    nips_data = load_data() 
 
     #initialize variables
     d = nips_data.shape[1]
@@ -139,19 +148,19 @@ if __name__ == "__main__":
 
         # E - step
         print("   E-step")
-        W = e_step(data=nips_data,k=3,mu=mus,pi=pi)
+        W = e_step(data=nips_data,k=k,mu=mus,pi=pi)
         
         # M - step
         print("   M-Step")
         mu_old = mus
-        mus = m_step(data=nips_data,W=W)
+        mus, pi = m_step_log(data=nips_data,W=W, pis=pi, alpha=10)
 
         #Measure
         converged = evalulate(mu_new=mus,mu_old=mu_old, threshold=threshold)
         
         iteration+=1
     
-    show_histogram(np.asarray(pis).T)
-    show_table(w_ij)
+    show_histogram(np.asarray(pi).T)
+    show_table(W)
 
         
